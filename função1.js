e8c6fbdf5b3287ed5aa70e8845cbd711b6d9520f
HIniku1 <gustavo.dreemurr@gmail.com>
2022-11-28 08:28:21 -0300

    Calculadora feita em JS

---
+++ função1.js
@@ -0,0 +1,20 @@
+const mais = function (a, b) {
+    return a + b;
+}
+console.log("adição: ", mais(5, 10));
+
+
+const div = function (a, b) {
+    return a / b;
+}
+console.log("divição: ", div(5, 10));
+
+const mult = function (a, b) {
+    return a * b;
+}
+console.log("multiplicação: ", mult(5, 10));
+
+const menos = function (a, b) {
+    return a - b;
+}
+console.log("subtração: ", menos(5, 10));
